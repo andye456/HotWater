cda86e2c6095213cbbbf3155431a4ef5268ac17f
Andy English <andye456@gmail.com>
2023-01-29 14:01:52 +0000

    added date range selection

--- app.py
+++ app.py
@@ -6,12 +6,21 @@ import time
 
 app = Flask(__name__)
 
+# Global vars to hold date ranges
+today = date.today().strftime("%Y-%m-%d")
+start = f"{today}T00:00:00.000"
+end = f"{today}T23:59:59.000"
+
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('main.html')
 
 @app.route('/get_time')
 def get_time():
+    """
+    Used to initialise the RPi Pico datetime as it as no onboard clock reference
+    :return:
+    """
     now = datetime.today().strftime("%Y,%m,%d,%w,%H,%M,%S,0")
     print(now)
     return now
@@ -24,8 +33,26 @@ def get_graph():
 
 @app.route('/level', methods=['GET', 'POST'])
 def get_level():
+    """
+    http://localhost:3000/level?start=2023-01-29T00:00:00&end=2023-01-29T10:00:00
+    :param range:
+    :return:
+    """
     name = "Tank Level Page!"
+    print(f"request.args = {request.args}")
+    try:
+        daterange=request.args.to_dict()
+        print(request.args.to_dict())
+        global start
+        start=daterange['start']
+        global end
+        end=daterange['end']
+        print(f"start = {start}")
+        print(f"end = {end}")
+    except:
+        pass
     return render_template('level.html')
+
 
 
 @app.route('/add', methods=['POST', 'GET'])
@@ -45,12 +72,7 @@ def get_data():
     database = mongo_client["hotwater"]
     temps = database["temp_data"]
 
-    today = date.today().strftime("%Y-%m-%d")
-
-    start = f"{today}T00:00:00.000"
-    end = f"{today}T23:59:59.000"
-
-    # data = {'data': temps.find_one()['data']}
+    print(f"Getting data with range: {start} to {end}")
     d = temps.find({'time':{'$gte': start,'$lt': end}}).sort('time')
     print(d)
     data = []
@@ -58,6 +80,14 @@ def get_data():
         for _e in _d['data']:
          data.append(_e)
     return {'data' : data}
+
+@app.route('/threshold', methods=['POST', 'GET'])
+def threshold():
+    """
+    Sets the threshold at which the water is detected as hot.
+    :return:
+    """
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
